6a986f613f2933e2849b932514bf70424c07fb34
Jacob Light <jakestar0306@comcast.net>
2017-09-22 15:55:48 -0400

    Added recipes (and an image for Mustard)
    Mustard, Cheese, Ketchup, Pickles, and Tomatoes

---
+++ Assets/Scripts/Recipes/CheeseRecipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseRecipe : Recipe
+{
+    //protected List<Event> events;
+
+    public CheeseRecipe(double startSongTime) : base(startSongTime)
+    {
+        image = Resources.Load<Sprite>("RecipeIcons/Cheese");
+        events.Add(new SoundEvent(startSongTime));
+        events.Add(new HitEvent(startSongTime + 2.5));
+    }
+}
---
+++ Assets/Scripts/Recipes/PicklesRecipe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicklesRecipe : Recipe
+{
+    //protected List<Event> events;
+
+    public PicklesRecipe(double startSongTime) : base(startSongTime)
+    {
+        image = Resources.Load<Sprite>("RecipeIcons/Pickles");
+        events.Add(new SoundEvent(startSongTime));
+        events.Add(new HitEvent(startSongTime + 2));
+        events.Add(new HitEvent(startSongTime + 3));
+        events.Add(new HitEvent(startSongTime + 4));
+        events.Add(new HitEvent(startSongTime + 4.5));
+        events.Add(new HitEvent(startSongTime + 5.5));
+    }
+}
