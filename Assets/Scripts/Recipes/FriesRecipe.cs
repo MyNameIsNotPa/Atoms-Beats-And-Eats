e757cd9ce07a6819e6f728c4a32e03bb10dbf05b
MyNameIsNotPa <drawinsewell@yahoo.com>
2017-10-20 15:53:26 -0400

    Fries and Milkshake have sounds

--- Assets/Scripts/Recipes/FriesRecipe.cs
+++ Assets/Scripts/Recipes/FriesRecipe.cs
@@ -8,6 +8,12 @@ public class FriesRecipe : Recipe
 		image = Resources.Load<Sprite> ("RecipeIcons/Fries");
 		events.Add (new SoundEvent (startSongTime));
 		events.Add (new HoldEvent(startSongTime + 2, startSongTime + 4));
+		events.Add (new SoundEvent(startSongTime + 2, Resources.Load<AudioClip>("Sounds/sizzle")));
+		events.Add (new SoundEvent(startSongTime + 4, Resources.Load<AudioClip>("Sounds/sizzle")));
+		events.Add (new HitEvent(startSongTime + 5));
+		events.Add (new SoundEvent(startSongTime + 5, Resources.Load<AudioClip>("Sounds/chop")));
+		events.Add (new HitEvent(startSongTime + 6));
+		events.Add (new SoundEvent(startSongTime + 6, Resources.Load<AudioClip>("Sounds/chop")));
 	}
 }
 
